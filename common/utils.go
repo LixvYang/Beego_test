f3158af9f3c50d321842501c9205dc5fe37f67e0
lixin <2690688423@qq.com>
2021-05-24 10:55:33 +0800

    update

---
+++ common/utils.go
@@ -0,0 +1,135 @@
+package common
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"math/rand"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/astaxie/beego"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/gomarkdown/markdown"
+	"github.com/hunterhug/go_image"
+	_ "github.com/jinhu/gorm"
+	"golang.org/x/text/currency"
+)
+
+//将时间戳转换为日期格式
+func TimestampToDate(timestamp int) string {
+	t := time.Unix(int64(timestamp),0)
+	return t.Format("2006-01-02 15:04:05")
+}
+
+//获取当前时间
+func GetUnix() int64 {
+	fmt.Println(time.Now().Unix())
+	return time.Now().Unix()
+}
+
+//获取时间戳的Nano时间
+func GetUnixNano() int64 {
+	return time.Now().UnixNano()		
+}
+
+func GetData() string {
+	template := "2006-01-02 15:04:05"
+	return time.Now().Format(template)
+}
+
+//md5加密
+func Md5(str string) string {
+	m := md5.New()
+	m.Write([]byte(str))
+	return string(hex.EncodeToString(m.Sum(nil)))
+}
+
+//验证邮箱
+func VerifyEmail(email string) bool {
+	pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+}
+
+//获取日期
+func FormatDay() string {
+	template := "20060102"
+	return time.Now().Format(template)
+}
+//生成订单号
+func GenerateOrderId() string {
+	template := "200601021504"
+	return time.Now().Format(template) + GetRandomNum()
+}
+//发送短信
+func SendMsg(str string)  {
+	//短信需要申请
+	//先固定一个值
+	ioutil.WriteFile("test_send.txt",[]byte(str),06666)
+}
+
+//重新剪裁图片
+func ResizeImage(filename string)  {
+	extName := path.Ext(filename)
+	resizeImage := strings.Split(beego.AppConfig.String("resizeImageSize"),",")
+
+	for i := 0;i<len(resizeImage);i++ {
+		w := resizeImage[i]
+		width,_ := strconv.Atoi(w)
+		savepath := filename + "_" + w + "x" + w + extName
+		err := go_image.ThumbnailF2F(filename,savepath,width,width)
+		if err != nil {
+			beego.Error(err)
+		}
+	}
+}
+
+//格式化图片
+func FormatImage(picName string) string {
+	ossStatus,err := beego.AppConfig.Bool("ossStatus")
+	if err != nil {
+		//判断当前目录前面是否有"/"
+		flag := strings.Contains(picName,"/static")
+		if flag {
+			return picName
+		}
+		return "/" + picName
+	}
+	if ossStatus {
+		return beego.AppConfig.String("ossDomain") + "/" + picName
+	} else {
+		flag := strings.Contains(picName,"/static")
+		if flag {
+			return picName
+		}
+		return "/" + picName
+	}
+}
+
+//格式化级标题
+func FOrmatAttribute(str string) string {
+	md := []byte(str)
+	htmlByte := markdown.ToHTML(md,nil,nil)
+	return string(htmlByte)
+}
+
+//计算乘法
+func Mul(price float64,num int) float64 {
+	return price * float64(num)
+}
+
+//封装一个产生随机数的方法
+func GetRandomNum() string {
+	var str string
+	for i := 0;i < 4;i++ {
+		current := rand.Intn(10) 
+		str += strconv.Itoa(current)
+	}
+	return str
+}
